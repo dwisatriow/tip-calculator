29bebb1e584f6750224074a040a292e556d070e0
Dwi Satrio W <31796624+dwisatriow@users.noreply.github.com>
2023-07-15 23:11:39 +0700

    updating item data

--- src/store/items/actions.js
+++ src/store/items/actions.js
@@ -1,5 +1,6 @@
 export const ITEM_ADDED = 'ITEM_ADDED';
 export const ITEM_REMOVED = 'ITEM_REMOVED';
+export const ITEM_PRICE_UPDATED = 'ITEM_PRICE_UPDATED';
 
 export const addNewItem = (name, price) => ({
   type: ITEM_ADDED,
@@ -13,5 +14,13 @@ export const removeItem = (uuid) => ({
   type: ITEM_REMOVED,
   payload: {
     uuid
+  }
+});
+
+export const updatePrice = (uuid, price) => ({
+  type: ITEM_PRICE_UPDATED,
+  payload: {
+    uuid,
+    price
   }
 });
